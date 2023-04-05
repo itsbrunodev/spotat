e57ec8a00a7cd21c91178c259d67af0c1ed30057
brunolepis <brunka215@gmail.com>
2023-04-06 00:00:19 +0200

    fix: remove fade in from spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,6 +1,6 @@
 export default function Spinner({ width = 40 }: { width?: number }) {
   return (
-    <div className="w-full h-full center animate__animated animate__fadeIn">
+    <div className="w-full h-full center">
       <div
         aria-hidden="true"
         className="inline-block border-white rounded-full border-2 border-b-black border-l-black animate-spin"
