8eec13a9f9afa7ded7afefc7c3d1fe29041179de
brunolepis <brunka215@gmail.com>
2023-04-05 20:49:24 +0200

    feat: add loading animation to login button

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,9 +1,13 @@
-export default function Spinner() {
+export default function Spinner({ width = 40 }: { width?: number }) {
   return (
     <div className="w-full h-full center animate__animated animate__fadeIn">
       <div
         aria-hidden="true"
-        className="inline-block border-white rounded-full border-2 border-b-black border-l-black w-10 h-10 animate-spin"
+        className="inline-block border-white rounded-full border-2 border-b-black border-l-black animate-spin"
+        style={{
+          width,
+          height: width,
+        }}
       ></div>
     </div>
   );
--- src/components/profile/Login.tsx
+++ src/components/profile/Login.tsx
@@ -1,14 +1,23 @@
 "use client";
 
 import { signIn } from "next-auth/react";
+import { useState } from "react";
+
+import Spinner from "../Spinner";
 
 export default function Login() {
+  const [loading, setLoading] = useState(false);
+
   return (
     <button
-      className="bg-spotify text-black hover:opacity-75 w-6/12 py-2 rounded-full font-medium"
-      onClick={() => signIn("spotify")}
+      className="bg-spotify text-black hover:opacity-75 w-6/12 py-2 rounded-full font-medium disabled:opacity-75 disabled:cursor-default"
+      disabled={loading}
+      onClick={() => {
+        setLoading(true);
+        signIn("spotify");
+      }}
     >
-      Login with Spotify
+      {loading ? <Spinner width={24} /> : <p>Login with Spotify</p>}
     </button>
   );
 }
