e4c757b8fb8ad2d809c7ce6d4efae3b46b4f2952
brunolepis <brunka215@gmail.com>
2023-04-05 23:45:21 +0200

    feat: improve error/bug reporting

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Error({ error }: { error: Error }) {
+  const pathName = usePathname();
+
   return (
     <div className="center flex-col w-full space-y-6 animate__animated animate__fadeIn">
       <div className="center w-full space-x-3 text-red-600 text-center">
@@ -25,7 +28,10 @@ export default function Error({ error }: { error: Error }) {
       </div>
       <Link
         className="bg-red-600 hover:opacity-75 px-5 py-2 rounded-full font-medium"
-        href="https://github.com/brunolepis/spotat/issues/new"
+        href={`https://github.com/brunolepis/spotat/issues/new?body=Pathname: \`${pathName}\`%0A\`\`\`%0A${error.stack?.replace(
+          "\n",
+          "%0A"
+        )}%0A\`\`\``}
         target="_blank"
       >
         Report Bug
