3f3b49bf57a64fc764a645a52cdf16ce2293a424
brunolepis <brunka215@gmail.com>
2023-04-06 15:40:15 +0200

    refactor: remove error icon

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -8,24 +8,9 @@ export default function Error({ error }: { error: Error }) {
 
   return (
     <div className="center flex-col w-full space-y-6 animate__animated animate__fadeIn">
-      <div className="center w-full space-x-3 text-red-600 text-center">
-        <svg
-          className="w-8"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          stroke-width="2"
-          stroke="currentColor"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-          <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
-          <path d="M12 8v4"></path>
-          <path d="M12 16h.01"></path>
-        </svg>
-        <h1 className="text-2xl font-semibold">{error.message}</h1>
-      </div>
+      <h1 className="text-2xl font-semibold text-red-600 text-center">
+        {error.message}
+      </h1>
       <Link
         className="bg-red-600 hover:opacity-75 px-5 py-2 rounded-full font-medium"
         href={`https://github.com/brunolepis/spotat/issues/new?body=Pathname: \`${pathName}\`%0A\`\`\`%0A${error.stack?.replace(
