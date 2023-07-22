081cb7c45b0cf2dc20ef7ed3a04c75089f5d52aa
brunolepis <brunka215@gmail.com>
2023-07-22 16:02:00 +0200

    chore: update deps

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
     serverComponentsExternalPackages: ["mongoose"],
+    serverActions: true,
   },
   images: {
     domains: ["i.scdn.co"],
